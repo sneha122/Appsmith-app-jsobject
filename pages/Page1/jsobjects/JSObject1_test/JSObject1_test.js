04e6f9b096ad3ffb1717eb8a34baeb759f60d594
cypress <cypress@appsmith.com>
2024-08-02 12:55:20 +0530

    new changes

--- pages/Page1/jsobjects/JSObject1_test/JSObject1_test.js
+++ pages/Page1/jsobjects/JSObject1_test/JSObject1_test.js
@@ -1,11 +1,11 @@
 export default {
 	myVar1: [],
 	myVar2: {},
-	myFun1 () {
+	myFun1_test () {
 		//	write code here
 		//	this.myVar1 = [1,2,3]
 	},
-	async myFun2 () {
+	async myFun2_refactor () {
 		//	use async-await or promises
 		//	await storeValue('varName', 'hello world')
 	}
